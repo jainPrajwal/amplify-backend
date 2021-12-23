bb183121ed0e9e45279e3085cd8602aa4726a630
prajwal jain <jainprajwal123@gmail.com>
2021-12-23 16:52:57 +0530

    feat : errorHandler and routeNotFoundHandler

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const { router: productsRouter } = require("./routes/products.router");
 const { router: cartRouter } = require("./routes/cart.router");
 const { router: signupRouter } = require("./routes/signup.router");
 const { router: loginRouter } = require("./routes/login.router");
+
+const {
+  routeNotFound,
+} = require("./middlewares/route-not-found-handler.middleware");
+const { errorHandler } = require("./middlewares/error-handler.middleware");
+
 const { initializeDatabase } = require("./db/db.connect");
 
 dotenv.config();
@@ -26,6 +32,11 @@ app.use("/products", productsRouter);
 app.use("/cart", cartRouter);
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
+
+// PLEASE DON'T MOVE.
+app.use(routeNotFound);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`listening to port :${PORT}`);
--- middlewares/authVerify.middleware.js
+++ middlewares/authVerify.middleware.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { findUserByUserId } = require("../utils/user.utils");
+
+
 const authVerify = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
